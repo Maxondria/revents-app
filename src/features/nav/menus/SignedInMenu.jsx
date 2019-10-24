f93799f6a694ca37d09c9b1bdb14ea7c21ecdb6b
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-25 00:17:57 +0300

    implement internal routing on settingsDashboard component

--- src/features/nav/menus/SignedInMenu.jsx
+++ src/features/nav/menus/SignedInMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu, Image, Dropdown } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const SignedInMenu = ({ signOut }) => {
   return (
@@ -11,12 +12,13 @@ const SignedInMenu = ({ signOut }) => {
           <Dropdown.Item text='My Events' icon='calendar' />
           <Dropdown.Item text='My Network' icon='users' />
           <Dropdown.Item text='My Profile' icon='user' />
-          <Dropdown.Item text='Settings' icon='settings' />
           <Dropdown.Item
-            text='Sign Out'
-            icon='power'
-            onClick={signOut}
+            text='Settings'
+            icon='settings'
+            as={Link}
+            to='/settings'
           />
+          <Dropdown.Item text='Sign Out' icon='power' onClick={signOut} />
         </Dropdown.Menu>
       </Dropdown>
     </Menu.Item>
--- src/features/user/settings/SettingsDashboard.jsx
+++ src/features/user/settings/SettingsDashboard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import SettingsNav from "./SettingsNav";
+import { Route, Switch, Redirect } from "react-router-dom";
+import BasicPage from "./BasicPage";
+import AboutPage from "./AboutPage";
+import PhotosPage from "./PhotosPage";
+import AccountPage from "./AccountPage";
 
 const SettingsDashboard = () => {
-  return <div>SettingsDashboard</div>;
+  return (
+    <Grid>
+      <Grid.Column width={12}>
+        <Switch>
+          {/* Redirect immediately to show basic settings here only if we are from /settings */}
+          <Redirect exact from='/settings' to='/settings/basic' />
+
+          <Route path='/settings/basic' component={BasicPage} />
+          <Route path='/settings/about' component={AboutPage} />
+          <Route path='/settings/photos' component={PhotosPage} />
+          <Route path='/settings/account' component={AccountPage} />
+        </Switch>
+      </Grid.Column>
+
+      <Grid.Column width={4}>
+        <SettingsNav />
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default SettingsDashboard;
