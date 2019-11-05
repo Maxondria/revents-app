5860432eb42bfd7d924457d335c104752cc74413
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-05 11:01:43 +0300

    upload photo action -rdx

--- src/app/redux/actions/userActions.js
+++ src/app/redux/actions/userActions.js
@@ -1,5 +1,10 @@
 import { toastr } from "react-redux-toastr";
 import { reset } from "redux-form";
+import {
+  asynActionStart,
+  asyncActionFinish,
+  asyncActionError
+} from "./asyncActions";
 
 export const updateProfile = user => async (
   dispatch,
@@ -17,5 +22,45 @@ export const updateProfile = user => async (
     toastr.success("Success!", "Profile Updated Successfully!");
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const uploadProfileImage = (file, filename) => async (
+  dispatch,
+  _getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+
+  const { uid, updateProfile } = firebase.auth().currentUser;
+  const path = `${uid}/user_images`;
+  const options = { filename };
+
+  try {
+    dispatch(asynActionStart());
+
+    const uploadedFile = await firebase.uploadFile(path, file, null, options);
+    const downloadURL = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+    const userRef = await firestore.get(`users/${uid}`);
+
+    if (!userRef.data().photoURL) {
+      await firebase.updateProfile({ photoURL: downloadURL });
+      await updateProfile({ photoURL: downloadURL });
+    }
+
+    await firestore.add(
+      {
+        collection: "users",
+        doc: uid,
+        subcollections: [{ collection: "photos" }]
+      },
+      { name: filename, url: downloadURL }
+    );
+
+    dispatch(asyncActionFinish());
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
   }
 };
