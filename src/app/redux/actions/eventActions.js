f73153203ed6e293b1dfd7f1df353ee71de5cb57
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-07 21:42:05 +0300

    clean fetch dummy api event action

--- src/app/redux/actions/eventActions.js
+++ src/app/redux/actions/eventActions.js
@@ -1,10 +1,3 @@
-import { FETCH_EVENTS } from "../constants/actionTypes";
-import {
-  asynActionStart,
-  asyncActionFinish,
-  asyncActionError
-} from "./asyncActions";
-import { mockFetch } from "../../mock-api/api";
 import { toastr } from "react-redux-toastr";
 import { createNewEvent } from "../../common/utils/helpers";
 
@@ -134,22 +127,5 @@ export const cancelEventToggle = (cancelled, eventId) => async (
     });
   } catch (error) {
     toastr.error("Ooops!", "Something Went Wrong!");
-  }
-};
-
-export const fetchEvents = () => async dispatch => {
-  try {
-    dispatch(asynActionStart());
-    const events = await mockFetch();
-    dispatch(asyncActionFinish());
-    dispatch({
-      type: FETCH_EVENTS,
-      payload: {
-        events
-      }
-    });
-  } catch (error) {
-    dispatch(asyncActionError());
-    console.log(error);
   }
 };
